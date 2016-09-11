b72d51f29f7b5ed10a324f5bbed2852be627b71d
vennela28 <vennela.jobs@gmail.com>
2016-09-10 19:00:27 -0700

    Use of Variables and Names

---
+++ VariablesAndNames/VariablesAndNames.java
@@ -0,0 +1,25 @@
+public class VariablesAndNames
+{
+    public static void main( String[] args )
+    {
+        int cars, drivers, passengers, cars_not_driven, cars_driven;
+        double space_in_a_car, carpool_capacity, average_passengers_per_car;
+
+        cars = 100;
+        space_in_a_car = 4;
+        drivers = 30;
+        passengers = 90;
+        cars_not_driven = cars - drivers;
+        cars_driven = drivers;
+        carpool_capacity = cars_driven * space_in_a_car;
+        average_passengers_per_car = passengers / cars_driven;
+
+
+        System.out.println( "There are " + cars + " cars available." );
+        System.out.println( "There are only " + drivers + " drivers available." );
+        System.out.println( "There will be " + cars_not_driven + " empty cars today." );
+        System.out.println( "We can transport " + carpool_capacity + " people today." );
+        System.out.println( "We have " + passengers + " to carpool today." );
+        System.out.println( "We need to put about " + average_passengers_per_car + " in each car." );
+    }
+}
