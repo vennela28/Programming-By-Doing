e97a1d92a5f65b8d75100e1fcc272bde6f231296
vennela28 <vennela.jobs@gmail.com>
2016-09-25 13:41:22 -0700

    Initials Of name

---
+++ YourInitials/YourInitials.java
@@ -0,0 +1,13 @@
+public class YourInitials{
+	public static void main(String[] args){
+		System.out.println("For your name Vennela Sunnam...");
+		System.out.println("12345    12345");
+		System.out.println("V   V  1 SSS  ");
+		System.out.println("V   V  2S    S");
+		System.out.println("V   V  3S     ");
+		System.out.println("V   V  4 SSS  ");
+		System.out.println("V   V  5     S");
+		System.out.println(" V V   6S    S");
+		System.out.println("  V    7 SSS  ");
+	}
+}
