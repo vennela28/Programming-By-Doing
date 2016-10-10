c13655bc331a1e95cc1971df9281b7d599da9fc4
vennela28 <vennela.jobs@gmail.com>
2016-10-10 14:02:51 -0700

    Using Variables

---
+++ UsingVariables/UsingVariables.java
@@ -0,0 +1,10 @@
+public class UsingVariables{
+	public static void main(String[] args){
+	    int room = 113;
+	    double val = 2.71828;
+	    String subject = "Computer Science";
+		System.out.println("This is room #"+room);
+		System.out.println("e is close to "+val);
+		System.out.println("I am learning a bit about "+subject);
+	}
+}
