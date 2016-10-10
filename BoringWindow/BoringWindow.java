4db49e3f116a26ee8f74969e9257d4a1be1f61be
vennela28 <vennela.jobs@gmail.com>
2016-10-10 15:56:05 -0700

    Boring Window

---
+++ BoringWindow/BoringWindow.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class BoringWindow extends JFrame{
+	public static void main(String[] args){
+		JFrame f = new BoringWindow();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(300,200);
+		f.setVisible(true);
+	}
+}
