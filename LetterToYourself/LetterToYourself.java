52b06ec495e91402593fbf9040ad116265bd4ce9
vennela28 <vennela.jobs@gmail.com>
2016-10-10 13:45:38 -0700

    Letter To Yourself

---
+++ LetterToYourself/LetterToYourself.java
@@ -0,0 +1,14 @@
+public class LetterToYourself{
+	public static void main(String args[]){
+		System.out.println("+--------------------------------------------------------------+");
+		System.out.println("|                                                         #### |");
+		System.out.println("|                                                         #### |");
+		System.out.println("|                                                         #### |");
+		System.out.println("|                                                              |");
+		System.out.println("|                             Sunnam Vennela                   |");
+		System.out.println("|                             1401 Red Hawk Circle, Apt-P204   |");
+		System.out.println("|                             Fremont, CA-94588                |");
+		System.out.println("|                                                              |");
+		System.out.println("+--------------------------------------------------------------+");
+	}
+}
