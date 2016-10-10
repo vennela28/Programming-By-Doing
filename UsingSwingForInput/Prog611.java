aca76b3d82d48088c3e69cd54dd5d024c7bf4358
vennela28 <vennela.jobs@gmail.com>
2016-10-10 15:52:28 -0700

    Input Box Domonstration using Swing

---
+++ UsingSwingForInput/Prog611.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Prog611{
+	public static void main(String[] args){
+		String name= JOptionPane.showInputDialog("What is your name?");
+		String input= JOptionPane.showInputDialog("How old are you?");
+		int age = Integer.parseInt(input);
+		
+		System.out.print("Hello, "+name+".");
+		System.out.print(" Next year, you\'ll be "+(age+1));
+		System.exit(0);
+	}
+}
