9bf941f92fafeade74ec501ee451f22c39b99695
vennela28 <vennela.jobs@gmail.com>
2016-10-10 16:54:59 -0700

    Guess the right PIN

---
+++ EnterPIN/EnterPIN.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class EnterPIN
+{
+	public static void main( String[] args )
+	{
+		Scanner keyboard = new Scanner(System.in);
+		int pin = 12345;
+
+		System.out.println("WELCOME TO THE BANK OF MITCHELL.");
+		System.out.print("ENTER YOUR PIN: ");
+		int entry = keyboard.nextInt();
+
+		while ( entry != pin )
+		{
+			System.out.println("\nINCORRECT PIN. TRY AGAIN.");
+			System.out.print("ENTER YOUR PIN: ");
+			entry = keyboard.nextInt();
+		}
+
+		System.out.println("\nPIN ACCEPTED. YOU NOW HAVE ACCESS TO YOUR ACCOUNT.");
+	}
+}
