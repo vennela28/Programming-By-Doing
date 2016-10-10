8f96a6decf69bbe8c06465ebc3f7971701e3c6b2
vennela28 <vennela.jobs@gmail.com>
2016-10-10 16:43:34 -0700

    One Shot Guess

---
+++ OneShotHiLo/OneShotHiLo.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class OneShotHiLo{
+	public static void main(String[] args){
+		Random r = new Random();
+		int x = 1+r.nextInt(100);
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("I\'m thinking of a number between 1-100.  Try to guess it.");
+		int guess = keyboard.nextInt();
+		System.out.println();
+		if(x==guess){
+			System.out.println("You guessed it!  What are the odds?!?");
+		}else if(x>guess){
+			System.out.println("Sorry, you are too low.  I was thinking of "+x+".");
+		}else if(x<guess){
+			System.out.println("Sorry, you are too high.  I was thinking of "+x+".");
+		}
+	}
+}
