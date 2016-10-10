12a8267bcaeb2d49f9b3e9156f979de818a6ed9b
vennela28 <vennela.jobs@gmail.com>
2016-10-10 15:47:50 -0700

    Random Numbers

---
+++ Randomness/Randomness.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+
+public class Randomness
+{
+	public static void main ( String[] args )
+	{
+		Random r = new Random();
+
+		int x = 1 + r.nextInt(10);
+
+		System.out.println( "My random number is " + x );
+
+		System.out.println( "Here are some numbers from 1 to 5!" );
+		System.out.print( 1 + r.nextInt(5) + " " );
+		System.out.print( 1 + r.nextInt(5) + " " );
+		System.out.print( 1 + r.nextInt(5) + " " );
+		System.out.print( 1 + r.nextInt(5) + " " );
+		System.out.print( 1 + r.nextInt(5) + " " );
+		System.out.print( 1 + r.nextInt(5) + " " );
+		System.out.println();
+
+		System.out.println( "Here are some numbers from 1 to 100!" );
+		System.out.print( 1 + r.nextInt(100) + "\t" );
+		System.out.print( 1 + r.nextInt(100) + "\t" );
+		System.out.print( 1 + r.nextInt(100) + "\t" );
+		System.out.print( 1 + r.nextInt(100) + "\t" );
+		System.out.print( 1 + r.nextInt(100) + "\t" );
+		System.out.print( 1 + r.nextInt(100) + "\t" );
+		System.out.println();
+
+		int num1 = 1 + r.nextInt(10);
+		int num2 = 1 + r.nextInt(10);
+
+		if ( num1 == num2 )
+		{
+			System.out.println( "The random numbers were the same! Weird." );
+		}
+		if ( num1 != num2 )
+		{
+			System.out.println( "The random numbers were different! Not too surprising, actually." );
+		}
+	}
+}
