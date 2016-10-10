b677a99046fa42b43045f006d2d656485e553ad3
vennela28 <vennela.jobs@gmail.com>
2016-10-10 14:53:57 -0700

    If Else Statements

---
+++ ElseAndIf/ElseAndIf.java
@@ -0,0 +1,47 @@
+public class ElseAndIf
+{
+	public static void main( String[] args )
+	{
+		int people = 30;
+		int cars = 40;
+		int buses = 15;
+
+		if ( cars > people )
+		{
+			System.out.println( "We should take the cars." );
+		}
+		else if ( cars < people )
+		{
+			System.out.println( "We should not take the cars." );
+		}
+		else
+		{
+			System.out.println( "We can't decide." );
+		}
+
+
+		if ( buses > cars )
+		{
+			System.out.println( "That's too many buses." );
+		}
+		else if ( buses < cars )
+		{
+			System.out.println( "Maybe we could take the buses." );
+		}
+		else
+		{
+			System.out.println( "We still can't decide." );
+		}
+
+
+		if ( people > buses )
+		{
+			System.out.println( "All right, let's just take the buses." );
+		}
+		else
+		{
+			System.out.println( "Fine, let's stay home then." );
+		}
+
+	}
+}
