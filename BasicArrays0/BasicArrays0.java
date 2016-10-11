a02d4ef43c61fd51934c81a5d261a49166adc5bd
vennela28 <vennela.jobs@gmail.com>
2016-10-10 22:08:40 -0700

    Basic Arrays without using a loop

---
+++ BasicArrays0/BasicArrays0.java
@@ -0,0 +1,16 @@
+public class BasicArrays0{
+	public static void main(String[] args){
+	int a[]={ -113, -113, -113, -113, -113, -113, -113, -113, -113, -113};
+	System.out.println("Slot 0 contains a "+a[0]);
+	System.out.println("Slot 1 contains a "+a[1]);
+	System.out.println("Slot 2 contains a "+a[2]);
+	System.out.println("Slot 3 contains a "+a[3]);
+	System.out.println("Slot 4 contains a "+a[4]);
+	System.out.println("Slot 5 contains a "+a[5]);
+	System.out.println("Slot 6 contains a "+a[6]);
+	System.out.println("Slot 7 contains a "+a[7]);
+	System.out.println("Slot 8 contains a "+a[8]);
+	System.out.println("Slot 9 contains a "+a[9]);
+	
+	}
+}
