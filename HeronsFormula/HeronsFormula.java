23f28e99bc61d4c297e78602fab474a00d49079a
vennela28 <vennela.jobs@gmail.com>
2016-10-10 19:31:45 -0700

    Hero Formula with Function

---
+++ HeronsFormula/HeronsFormula.java
@@ -0,0 +1,33 @@
+public class HeronsFormula
+{
+	public static void main( String[] args )
+	{
+		double a;
+		
+		a = triangleArea(3, 3, 3);
+		System.out.println("A triangle with sides 3,3,3 has an area of " + a );
+
+		a = triangleArea(3, 4, 5);
+		System.out.println("A triangle with sides 3,4,5 has an area of " + a );
+ 
+		a = triangleArea(7, 8, 9);
+		System.out.println("A triangle with sides 7,8,9 has an area of " + a );
+
+		System.out.println("A triangle with sides 5,12,13 has an area of " + triangleArea(5, 12, 13) );
+		System.out.println("A triangle with sides 10,9,11 has an area of " + triangleArea(10, 9, 11) );
+		System.out.println("A triangle with sides 8,15,17 has an area of " + triangleArea(8, 15, 17) );
+		System.out.println("A triangle with sides 9,9,9 has an area of " + triangleArea(9, 9, 9) );
+	}
+ 
+	public static double triangleArea( int a, int b, int c )
+	{
+		// the code in this function computes the area of a triangle whose sides have lengths a, b, and c
+		double s, A;
+
+		s = (a+b+c) / 2.0;
+		A = Math.sqrt( s*(s-a)*(s-b)*(s-c) );
+
+		return A;
+		// ^ after computing the area, "return" it
+	}
+}
