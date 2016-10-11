c668d50443096377f7b807b6a5d66ae58aa35020
vennela28 <vennela.jobs@gmail.com>
2016-10-10 17:54:10 -0700

    Multiplication Table

---
+++ MultiplicationTable/MultiplicationTable.java
@@ -0,0 +1,20 @@
+public class MultiplicationTable{
+	public static void main(String[] args){
+	System.out.println("x | 1      2      3      4      5      6       7       8       9 ");
+	System.out.println("==+===============================================================");
+	for(int i=1; i<=12; i++){
+		System.out.print(i+" | ");
+		
+		for(int j=1; j<=9; j++){
+			System.out.print(j*i+"      ");
+			
+		
+		
+		
+		
+	}System.out.println();
+		
+	
+	}
+}
+}
