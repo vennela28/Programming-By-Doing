d3765031f2d3d6ef921bc75c0dfe322169ad863f
vennela28 <vennela.jobs@gmail.com>
2016-10-10 17:08:16 -0700

    Correct PIN guess in three tries

---
+++ PinLockout/PinLockout.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PinLockout
+{
+	public static void main( String[] args )
+	{
+		Scanner keyboard = new Scanner(System.in);
+		int pin = 12345;
+		int tries = 0;
+
+		System.out.println("WELCOME TO THE BANK OF MITCHELL.");
+		System.out.print("ENTER YOUR PIN: ");
+		int entry = keyboard.nextInt();
+		tries++;
+
+		while ( entry != pin && tries < 3 )
+		{
+			System.out.println("\nINCORRECT PIN. TRY AGAIN.");
+			System.out.print("ENTER YOUR PIN: ");
+			entry = keyboard.nextInt();
+			tries++;
+		}
+
+		if ( entry == pin )
+			System.out.println("\nPIN ACCEPTED. YOU NOW HAVE ACCESS TO YOUR ACCOUNT.");
+		else if ( tries >= 3 )
+			System.out.println("\nYOU HAVE RUN OUT OF TRIES. ACCOUNT LOCKED.");
+	}
+}
