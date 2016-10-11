9788c685c43683278dfc2e339fe3ccc31ff0aa33
vennela28 <vennela.jobs@gmail.com>
2016-10-10 20:57:46 -0700

    Display Prime Numbers between 1-100

---
+++ FindingPrimeNumbers/FindingPrimeNumbers.java
@@ -0,0 +1,17 @@
+public class FindingPrimeNumbers{
+	public static void main(String[] args){
+		for(int i=2; i<=99; i++){
+			if(isPrime(i)){
+				System.out.println(i);
+			}
+		}
+	
+	}public static boolean isPrime(int n){
+		boolean prime=false;
+		for(int i=2; i<=n/2; i++){
+			 if(n % i == 0){
+                return false;
+			 }
+		}return true;
+	}
+}
