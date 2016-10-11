6fcdcbdaafeca521ca5a86bca00e804a4e78d110
vennela28 <vennela.jobs@gmail.com>
2016-10-10 18:03:29 -0700

    Collatz Sequence

---
+++ CollatzSequence/CollatzSequence.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class CollatzSequence{
+	public static void main(String[] args){
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Starting number: ");
+		int start = keyboard.nextInt();
+		int count = 0;
+		int max=0;
+		max=start;
+		System.out.print(start+"	");
+	while(start!=1){
+		if(start%2==0){
+				start=start/2;
+		}else{
+				start= 3*start+1;
+		}
+			System.out.print(start+"	");
+			count++;
+		if(start>max){
+				max = start;
+		}	
+		if(count%10==0){
+			
+		}
+	}
+		System.out.println();
+		System.out.println();
+		System.out.println("The largest value was "+max+".");
+		System.out.println("Terminated after "+count+" steps.");
+	}
+}
