fc9eb8f3ac3ad70ee35a0f8815e0c95bd20796df
vennela28 <vennela.jobs@gmail.com>
2016-10-11 08:06:35 -0700

    Demonstration of arrays using loops

---
+++ BasicArrays1/BasicArrays1.java
@@ -0,0 +1,8 @@
+public class BasicArrays1{
+	public static void main(String[] args){
+	int a[]=new int[10];
+	for(int i=0; i<10; i++){
+		System.out.println("Slot "+i+" contains a -113");
+	}
+	}
+}
