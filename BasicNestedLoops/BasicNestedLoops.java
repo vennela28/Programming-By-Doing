b0e916a10ff32087fbb65e49c1fff8b5c6845ab6
vennela28 <vennela.jobs@gmail.com>
2016-10-10 21:28:03 -0700

    Basic Nested Loops

---
+++ BasicNestedLoops/BasicNestedLoops.java
@@ -0,0 +1,10 @@
+public class BasicNestedLoops{
+	public static void main(String[] args){
+	for(int i =0; i<=5; i++){
+		System.out.println();
+		for(int j =0; j<=5; j++){
+			System.out.print("("+i+","+j+")  ");
+			
+	}}
+	}
+}
