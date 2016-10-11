dfcfc1e343e7348ce52ca983f1dcce42127bdb8b
vennela28 <vennela.jobs@gmail.com>
2016-10-10 18:50:57 -0700

    Count by Halves

---
+++ CountingByHalves/CountingByHalves.java
@@ -0,0 +1,11 @@
+public class CountingByHalves{
+	public static void main(String[] args){
+	System.out.println("x");
+	System.out.println("------");
+	
+	for(double i=-10.0; i<=10.0; i+=0.5){
+		System.out.println(i);
+		//System.out.println(i);
+	}
+	}
+}
