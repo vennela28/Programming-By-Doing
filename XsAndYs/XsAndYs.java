c3c8888fb50ce729426910a7c1cce45309e6dacc
vennela28 <vennela.jobs@gmail.com>
2016-10-10 18:53:27 -0700

    Display the squares of negative numbers too

---
+++ XsAndYs/XsAndYs.java
@@ -0,0 +1,9 @@
+public class XsAndYs{
+	public static void main(String[] args){
+	System.out.println("x		y    ");
+	System.out.println("-------------------------");
+	for(double i=-10.0; i<=10.0; i++){
+		System.out.println(i+"		"+(i*i));
+	}
+	}
+}
