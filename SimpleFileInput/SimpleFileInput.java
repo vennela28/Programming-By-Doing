00970db48bdef0bd826e9d767e9ebc4e9fc1eb38
vennela28 <vennela.jobs@gmail.com>
2016-10-10 21:58:25 -0700

    Grab the name from file name.txt and attach it to the text

---
+++ SimpleFileInput/SimpleFileInput.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.io.File;
+
+public class SimpleFileInput
+{
+	public static void main( String[] args ) throws Exception
+	{
+		
+		Scanner fileIn = new Scanner(new File("name.txt"));
+		String l_name =fileIn.next();
+		String f_name =fileIn.next();
+		
+		System.out.println( "Using my psychic powers aided by reading data from the file, I have" );
+		System.out.println( "determined that your name is " +f_name+" "+l_name+"." );
+		System.out.println( "Amazing, huh?" );
+	}
+}
