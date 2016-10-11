46b3423a5f91e17d35113611999a90e2c61033a5
vennela28 <vennela.jobs@gmail.com>
2016-10-10 21:25:34 -0700

    Odometer Reading

---
+++ OdometerLoops/OdometerLoops.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class OdometerLoops
+{
+	public static void main( String[] args ) throws Exception
+	{
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Which base (2-7): ");
+		int n =keyboard.nextInt();
+		for ( int thous=0; thous<n; thous++ )
+		
+			for ( int hund=0; hund<n; hund++ )
+			
+				for ( int tens=0; tens<n; tens++ )
+			
+					for ( int ones=0; ones<n; ones++ )
+					{
+						System.out.print( " " + thous + "" + hund + "" + tens + "" + ones + "\r" );
+						Thread.sleep(500);
+					}
+				
+			
+		
+
+		System.out.println();
+	}
+}
