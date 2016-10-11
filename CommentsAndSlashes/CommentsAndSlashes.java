452aa819f06cc059967205ab50e6a407f0655505
vennela28 <vennela.jobs@gmail.com>
2016-10-10 17:10:35 -0700

    Comments and Slashes Demo

---
+++ CommentsAndSlashes/CommentsAndSlashes.java
@@ -0,0 +1,15 @@
+public class CommentsAndSlashes
+{
+    public static void main( String[] args )
+    {
+        // A comment, this is so you can read your program later.
+        // Anything after the // is ignored by Java.
+
+        System.out.println( "I could have code like this." ); // and the comment after is ignored.
+
+        // You can also use a comment to "disable" or comment out a piece of code:
+        // System.out.println("This won't run.");
+
+        System.out.println( "This will run." );
+    }
+}
