f5df99a290554990891748fe008d094fa7738d74
vennela28 <vennela.jobs@gmail.com>
2016-10-10 17:43:01 -0700

    Number Guessing with a do-while loop counter

---
+++ AgainWithTheNumberGuessing/AgainWithTheNumberGuessing.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class AgainWithTheNumberGuessing{
+	public static void main(String[] args){
+	Scanner keybaord = new Scanner(System.in);
+	Random r = new Random();
+	int actual= r.nextInt(10);
+	System.out.println("I have chosen a number between 1 and 10.  Try to guess it.");
+	int tries=0;
+	System.out.print("Your guess: ");
+	int guess = keybaord.nextInt();
+	do{
+		if(guess!=actual){
+			System.out.println("That is incorrect. Guess again.");
+			System.out.print("Your guess: ");
+			guess = keybaord.nextInt();
+			tries++;
+		}
+	}while(guess!=actual);
+			System.out.println("That's right!  You're a good guesser.");
+			System.out.println("It took you "+tries+" tries.");		
+	}
+}
