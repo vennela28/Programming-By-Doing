a9b90fca65b646f690895e2eb51b4c56f6bc83fe
vennela28 <vennela.jobs@gmail.com>
2016-10-11 08:20:34 -0700

    Use Array List without a loop demo

---
+++ BasicArrayLists0/BasicArrayLists0.java
@@ -0,0 +1,19 @@
+import java.util.*;
+@SuppressWarnings("unchecked")
+public class BasicArrayLists0{
+	public static void main(String[] args){
+	ArrayList listTest = new ArrayList();
+		listTest.add( "-113" );
+		
+        System.out.println( "Slot 0 contains a " + listTest.get(0) );
+		System.out.println( "Slot 1 contains a " + listTest.get(0) );
+		System.out.println( "Slot 2 contains a " + listTest.get(0) );
+		System.out.println( "Slot 3 contains a " + listTest.get(0) );
+		System.out.println( "Slot 4 contains a " + listTest.get(0) );
+		System.out.println( "Slot 5 contains a " + listTest.get(0) );
+		System.out.println( "Slot 6 contains a " + listTest.get(0) );
+		System.out.println( "Slot 7 contains a " + listTest.get(0) );
+		System.out.println( "Slot 8 contains a " + listTest.get(0) );
+		System.out.println( "Slot 9 contains a " + listTest.get(0) );
+	}
+}
