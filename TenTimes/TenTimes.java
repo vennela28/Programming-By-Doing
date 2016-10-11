31973cec47dc25ddc4d3ef39d9d7139eac7b50a1
vennela28 <vennela.jobs@gmail.com>
2016-10-10 18:27:51 -0700

    Ten times in a for loop

---
+++ TenTimes/TenTimes.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class TenTimes{
+	public static void main(String[] args){
+		for(int i=0; i<10; i++){
+			System.out.println((i+1)+". Mr. Mitchell is cool.");
+		}
+	}
+}
