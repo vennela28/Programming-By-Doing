de4be372e67bd0a80ef164d2da0f835906b90d57
vennela28 <vennela.jobs@gmail.com>
2016-10-10 17:04:18 -0700

    While loop Demonstration

---
+++ CountingWhile/CountingWhile.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CountingWhile
+{
+	public static void main( String[] args )
+	{
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.println( "Type in a message, and I'll display it five times." );
+		System.out.print( "Message: " );
+		String message = keyboard.nextLine();
+
+		int n = 0;
+		while ( n < 5 )
+		{
+			System.out.println( (n+1) + ". " + message );
+			n++;
+		}
+
+	}
+}
