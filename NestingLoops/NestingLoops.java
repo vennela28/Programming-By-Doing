b1c39a45e62cd0198b4e4639224922d4a13aed19
vennela28 <vennela.jobs@gmail.com>
2016-10-10 21:22:13 -0700

    Nested Loops

---
+++ NestingLoops/NestingLoops.java
@@ -0,0 +1,30 @@
+public class NestingLoops
+{
+	public static void main( String[] args )
+	{
+		// this is #1 - I'll call it "CN"
+		for ( int n=1; n <= 3; n++ )
+		
+		{
+			for ( char c='A'; c <= 'E'; c++ )
+			{
+				System.out.println( c + " " + n );
+			}
+		}
+
+		System.out.println("\n");
+
+		// this is #2 - I'll call it "AB"
+		for ( int a=1; a <= 3; a++ )
+		{
+			for ( int b=1; b <= 3; b++ )
+			{
+				System.out.print( a + "-" + b + " " );
+			}
+			// * You will add a line of code here.
+		}
+
+		System.out.println("\n");
+
+	}
+}
