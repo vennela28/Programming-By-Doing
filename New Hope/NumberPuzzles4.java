df5eebf0ebd5124bd49e33ddd0f317466ce4675e
vennela28 <vennela.jobs@gmail.com>
2016-10-10 21:44:30 -0700

    Puzzle with 4 Numbers

---
+++ New Hope/NumberPuzzles4.java
@@ -0,0 +1,14 @@
+public class NumberPuzzles4{
+	public static void main(String[] args){
+	for(int a=1; a<45; a++){
+		for(int b=1; b<45; b++){
+			for(int c=1; c<45; c++){
+				for(int d=1; d<45; d++){
+					if ( (a + b + c + d) == 45 && (a + 2) == (b - 2) && (b - 2) == (c * 2) && (c *2) == (d / 2) )
+						System.out.println( a + " + " + b + " + " + c + " + " + d );
+				}
+			}
+		}
+	}
+	}
+}
