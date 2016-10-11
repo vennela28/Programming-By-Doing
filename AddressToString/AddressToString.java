ef8edaef1feb10327228c24836905beb4f5c102f
vennela28 <vennela.jobs@gmail.com>
2016-10-11 12:28:31 -0700

    Address to string records from a file

---
+++ AddressToString/AddressToString.java
@@ -0,0 +1,44 @@
+import java.net.URL;
+import java.util.Scanner;
+
+class Address
+{
+	String street;
+	String city;
+	String state;
+	int zip;
+
+	public String toString()
+	{
+		return (this.street + ", " + this.city + "  " + this.zip);
+	}
+}
+
+public class AddressToString
+{
+	public static void main(String[] args) throws Exception
+	{
+		URL addys = new URL("https://cs.leanderisd.org/txt/fake-addresses.txt");
+
+		Address[] addybook = new Address[5];
+
+		Scanner fin = new Scanner( addys.openStream() );
+
+		for ( int i=0; i<5; i++ )
+		{
+			addybook[i] = new Address();
+			addybook[i].street = fin.nextLine();
+			addybook[i].city   = fin.nextLine();
+			addybook[i].state  = fin.next();
+			addybook[i].zip    = fin.nextInt();
+			fin.skip("\n");
+		}
+		fin.close();
+		
+		for ( int i=0; i<5; i++ )
+		{
+			System.out.println( (i+1) + ". " + addybook[i] );
+		}
+	}
+}
+
