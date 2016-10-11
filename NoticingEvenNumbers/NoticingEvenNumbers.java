e6959c376d3f6fc76e693a36fc24f17fecb2f26e
vennela28 <vennela.jobs@gmail.com>
2016-10-10 19:02:34 -0700

    Even Numbers Display

---
+++ NoticingEvenNumbers/NoticingEvenNumbers.java
@@ -0,0 +1,11 @@
+public class NoticingEvenNumbers{
+	public static void main(String[] args){
+		for(int i=1; i<=20; i++){
+			if(i%2==0){
+				System.out.println(i+" <");
+			}else{
+				System.out.println(i);
+			}
+		}
+	}
+}
