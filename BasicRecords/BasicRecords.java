e95f02e6091ed16328343202b402e8078659898b
vennela28 <vennela.jobs@gmail.com>
2016-10-11 20:32:20 -0700

    Basic Reacords Display

---
+++ BasicRecords/BasicRecords.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+class Student
+{
+	String name;
+	int grade;
+	double avg;
+}
+public class BasicRecords{
+	public static void main(String[] args){
+		Scanner keyboard = new Scanner(System.in);
+		Student s_one = new Student();
+		Student s_two = new Student();
+		Student s_three = new Student();
+		System.out.print("Enter the first student's name: ");
+		 s_one.name = keyboard.next();
+		System.out.print("Enter the first student's grade: ");
+		 s_one.grade = keyboard.nextInt();
+		System.out.print("Enter the first student's average: ");
+		 s_one.avg = keyboard.nextDouble();
+		System.out.println();
+		System.out.print("Enter the second student's name: ");
+		 s_two.name = keyboard.next();
+		System.out.print("Enter the second student's grade: ");
+		 s_two.grade = keyboard.nextInt();
+		System.out.print("Enter the second student's average: ");
+		 s_two.avg = keyboard.nextDouble();
+		System.out.println();
+		System.out.print("Enter the third student's name: ");
+		s_three.name = keyboard.next();
+		System.out.print("Enter the third student's grade: ");
+		s_three.grade = keyboard.nextInt();
+		System.out.print("Enter the third student's average: ");
+		s_three.avg = keyboard.nextDouble();
+		System.out.println();
+	
+		System.out.println("The names are: "+s_one.name+" "+s_two.name+" "+s_three.name);
+		System.out.println("The grades are: "+s_one.grade+" "+s_two.grade+" "+s_three.grade);
+		System.out.println("The averages are: "+s_one.avg+" "+s_two.avg+" "+s_three.avg);
+		System.out.println();
+		double total = s_one.avg + s_two.avg + s_three.avg;
+		double total_avg = total/3.0;
+		
+		System.out.printf("The average for the three students is: "+"%.6f", total_avg);
+	}
+}
