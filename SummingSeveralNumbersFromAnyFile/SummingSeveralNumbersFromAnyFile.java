9fb1732e67387e231794d37577772060faa3c2f8
vennela28 <vennela.jobs@gmail.com>
2016-10-20 10:48:02 -0700

    Sum several numbers from a file specified

--- SummingSeveralNumbersFromAnyFile/SummingSeveralNumbersFromAnyFile.java
+++ SummingSeveralNumbersFromAnyFile/SummingSeveralNumbersFromAnyFile.java
@@ -6,21 +6,16 @@ public class SummingSeveralNumbersFromAnyFile{
 	    String usrFile;
 		System.out.print( "Which file would you like to read numbers from: " );
 		usrFile = keyboard.next();
-		
 		File in = new File(usrFile);
 		Scanner input = new Scanner(in);
-		
 		System.out.println( "Reading numbers from file \"" + usrFile + "\"\n" );
-		
 		int total = 0;
-		
 		while ( input.hasNext() )
 		{
 		    int num = input.nextInt();
 		    System.out.print( num + " " );
 		    total += num;		
 		}
-		
 		System.out.println( "\nTotal is " + total);
 	}
 }
