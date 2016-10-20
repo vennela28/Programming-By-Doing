b7dde92ce9d3baff77fae48db5df5cbbdd8cd72c
vennela28 <vennela.jobs@gmail.com>
2016-10-20 08:58:59 -0700

    Sum severla numbers from any file

---
+++ SummingSeveralNumbersFromAnyFile/SummingSeveralNumbersFromAnyFile.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+public class SummingSeveralNumbersFromAnyFile{
+	public static void main(String args[]) throws Exception{
+		Scanner keyboard = new Scanner(System.in);
+	    String usrFile;
+		System.out.print( "Which file would you like to read numbers from: " );
+		usrFile = keyboard.next();
+		
+		File in = new File(usrFile);
+		Scanner input = new Scanner(in);
+		
+		System.out.println( "Reading numbers from file \"" + usrFile + "\"\n" );
+		
+		int total = 0;
+		
+		while ( input.hasNext() )
+		{
+		    int num = input.nextInt();
+		    System.out.print( num + " " );
+		    total += num;		
+		}
+		
+		System.out.println( "\nTotal is " + total);
+	}
+}
