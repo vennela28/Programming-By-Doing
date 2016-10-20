95c4953776f8a064979546b0c6539032a1125270
vennela28 <vennela.jobs@gmail.com>
2016-10-20 11:20:43 -0700

    Display a file

---
+++ DisplayingAFile/DisplayingAFile.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class DisplayingAFile{
+	public static void main(String[] args) throws Exception{
+	Scanner keyboard = new Scanner(System.in);
+	System.out.print( "Which file would you like to read numbers from: " );
+		String usrFile = keyboard.next();
+		
+	File in = new File(usrFile);
+	Scanner input = new Scanner(in);
+		
+		while ( input.hasNext() )
+		{
+		    System.out.println( input.nextLine() );
+}
+	
+	}
+}
