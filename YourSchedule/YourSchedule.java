2312e49f3e7dd0476980a0c12339f95871f9d254
vennela28 <vennela.jobs@gmail.com>
2017-08-30 21:45:24 -0700

    Changes to Schedule look

--- YourSchedule/YourSchedule.java
+++ YourSchedule/YourSchedule.java
@@ -3,14 +3,14 @@ public class YourSchedule{
 		String course1="Maths", course2="Science", course3="SST", course4="English", course5="Hindi", course6="Sanskrit", course7="Biology";
 		String teacher1="Mrs. Pratima", teacher2="Mr.Ramesh", teacher3="Mrs.Rana", teacher4="Mrs.Manjula", teacher5="Mr.Tiwari", teacher6="Mrs.Mali", teacher7="Mrs.Singh";
 		
-		System.out.println("+----------------------------------------------------------------------+");
-		System.out.println("| 1 |                                      "+course1+"   |   "+teacher1+"    |");
-		System.out.println("| 2 |                                      "+course2+" |   "+teacher2+"       |");
-		System.out.println("| 3 |                                      "+course3+"     |   "+teacher3+"        |");
-		System.out.println("| 4 |                                      "+course4+" |   "+teacher4+"     |");
-		System.out.println("| 5 |                                      "+course5+"   |   "+teacher5+"       |");
-		System.out.println("| 6 |                                      "+course6+"|   "+teacher6+"        |");
-		System.out.println("| 7 |                                      "+course7+" |   "+teacher7+"       |");
-		System.out.println("+----------------------------------------------------------------------+");
+		System.out.println("+--------------------------------+");
+		System.out.println("| 1 |     "+course1+" |   "+teacher1+" |");
+		System.out.println("| 2 |   "+course2+" |      "+teacher2+" |");
+		System.out.println("| 3 |       "+course3+" |       "+teacher3+" |");
+		System.out.println("| 4 |   "+course4+" |    "+teacher4+" |");
+		System.out.println("| 5 |     "+course5+" |      "+teacher5+" |");
+		System.out.println("| 6 |  "+course6+" |       "+teacher6+" |");
+		System.out.println("| 7 |   "+course7+" |      "+teacher7+" |");
+		System.out.println("+--------------------------------+");
 	}
 }
