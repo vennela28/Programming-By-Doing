968f96c2e7c03c7cbd3bd5f16cb7dbe76edcb513
vennela28 <vennela.jobs@gmail.com>
2017-09-08 14:58:34 -0700

    Change in the loop logic

--- AgeMessages3/AgeMessages3.java
+++ AgeMessages3/AgeMessages3.java
@@ -12,7 +12,7 @@ public class AgeMessages3{
 		System.out.println("You can't drive, "+name+".");
 	}if(age>=16 && age<=17){
 		System.out.println("You can drive but not vote, "+name+".");
-	}if(age>=18 && age<24){
+	}if(age>=18 && age<=24){
 		System.out.println("You can vote but not rent a car, "+name+".");
 	}if(age>=25){
 		System.out.println("You can do anything that's legal, "+name+".");
