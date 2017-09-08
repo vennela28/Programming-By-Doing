3a599c2052c7e3f769397504289ef3faddf8e25e
vennela28 <vennela.jobs@gmail.com>
2017-09-08 15:35:34 -0700

    compareTo() method Challenge

--- CompareToChallenge/CompareToChallenge.java
+++ CompareToChallenge/CompareToChallenge.java
@@ -10,17 +10,50 @@ public class CompareToChallenge{
 	String str1 = "Strings are immutable";
 	String str2 = "Strings are immutable";
     String str3 = "Integers are not immutable";
+	String str4 = "Tesla";
+	String str5 = "Benz";
+	String str6 = "Lexus";
+	String str7 = "BMW";
+	String str8 = "Zen";
 
+    System.out.print("Comparing \"Strings are immutable\" with \"Strings are immutable\" produces ");
     int result = str1.compareTo( str2 );
-    System.out.println(result);
+	System.out.println(result);
 	  
+	System.out.print("Comparing \"Strings are immutable\" with \"Strings are immutable\" produces ");
     result = str2.compareTo( str3 );
     System.out.println(result);
 	 
-    result = str3.compareTo( str1 );
+	System.out.print("Comparing \"Tesla\" with \"Benz\" produces ");
+    result = str4.compareTo( str5 );
     System.out.println(result);
 	
+	System.out.print("Comparing \"Benz\" with \"Tesla\" produces ");
+	result = str5.compareTo( str4 );
+    System.out.println(result);
 	
+	System.out.print("Comparing \"Lexus\" with \"BMW\" produces ");
+	result = str6.compareTo( str7 );
+    System.out.println(result);
 	
+	System.out.print("Comparing \"BMW\" with \"Zen\" produces ");
+	result = str7.compareTo( str8 );
+    System.out.println(result);
+	
+	System.out.print("Comparing \"BMW\" with \"Tesla\" produces ");
+	result = str7.compareTo( str4 );
+    System.out.println(result);
+	
+	System.out.print("Comparing \"Benz\" with \"Lexus\" produces ");
+	result = str5.compareTo( str6 );
+    System.out.println(result);
+	
+	System.out.print("Comparing \"Lexus\" with \"Benz\" produces ");
+	result = str6.compareTo( str5 );
+    System.out.println(result);
+	
+	System.out.print("Comparing \"BMW\" with \"BMW\" produces ");
+	result = str7.compareTo( str7 );
+    System.out.println(result);
 	}
 }
