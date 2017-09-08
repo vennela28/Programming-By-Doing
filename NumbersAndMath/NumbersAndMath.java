0ff60b699650f715c22341ffa317b00ce018d407
vennela28 <vennela.jobs@gmail.com>
2017-09-08 15:11:53 -0700

    Mathematics

--- NumbersAndMath/NumbersAndMath.java
+++ NumbersAndMath/NumbersAndMath.java
@@ -4,7 +4,7 @@ public class NumbersAndMath
 	{
 		System.out.println( "I will now count my chickens:" );
 
-		System.out.println( "Hens " + ( 25.0 + 30.0 / 6 ) );
+		System.out.println( "Hens " + ( 25+ 30 / 6 ) );
 		System.out.println( "Roosters " + ( 100 - 25 * 3 % 4 ) );
 
 		System.out.println( "Now I will count the eggs:" );
