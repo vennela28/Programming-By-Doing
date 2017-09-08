0a21f098d595a08754175a8cc921d788923639c5
vennela28 <vennela.jobs@gmail.com>
2017-09-08 14:13:37 -0700

    Modify to test Git

--- FirstProg/FirstProg.java
+++ FirstProg/FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg
 {
 	public static void main(String[] args){
-		System.out.println("Vennela dont lose hope! Have focus to start from the beginning.");
+		System.out.println("Vennela don't lose hope! Have focus to start from the beginning.");
 	}
 }
