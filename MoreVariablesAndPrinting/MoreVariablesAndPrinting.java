e8264ea347f194882edce3f407ac6dc1db1c65db
vennela28 <vennela.jobs@gmail.com>
2017-09-08 15:29:32 -0700

    Variables

--- MoreVariablesAndPrinting/MoreVariablesAndPrinting.java
+++ MoreVariablesAndPrinting/MoreVariablesAndPrinting.java
@@ -3,8 +3,8 @@ public class MoreVariablesAndPrinting
     public static void main( String[] args )
     {
         String Name, Eyes, Teeth, Hair;
-        int Age;
-		double Height, Weight;
+        int Age, Height, Weight;
+		double myHeight, myWeight;
 
         Name = "Zed A. Shaw";
         Age = 35;     // not a lie
@@ -13,17 +13,17 @@ public class MoreVariablesAndPrinting
         Eyes = "Blue";
         Teeth = "White";
         Hair = "Brown";
-		Weight = 0.4535*Weight;
-		Height = 2.54*Height;
+		myWeight = 0.4535*Weight;
+		myHeight = 2.54*Height;
         System.out.println( "Let's talk about " + Name + "." );
-        System.out.println( "He's " + Height + " inches tall." );
-        System.out.println( "He's " + Weight + " pounds heavy." );
+        System.out.println( "He's " + Height + " inches (or "+myHeight+ " cm) tall." );
+        System.out.println( "He's " + Weight + " lbs (or "+Math.round(myWeight)+ " kg) heavy." );
         System.out.println( "Actually, that's not too heavy." );
         System.out.println( "He's got " + Eyes + " eyes and " + Hair + " hair." );
         System.out.println( "His teeth are usually " + Teeth + " depending on the coffee." );
 
         // This line is tricky; try to get it exactly right.
         System.out.println( "If I add " + Age + ", " + Height + ", and " + Weight
-            + " I get " + (Age + Height + Weight) + "." );
+            + " I get " + Math.round(Age + Height + Weight) + "." );
     }
 }
