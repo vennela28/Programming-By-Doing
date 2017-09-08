6e9d34d4e64c6130f852e1fb9d20d732ac52819b
vennela28 <vennela.jobs@gmail.com>
2017-09-08 14:59:31 -0700

    Change in the intervals for decimals

--- XsAndYs/XsAndYs.java
+++ XsAndYs/XsAndYs.java
@@ -2,7 +2,7 @@ public class XsAndYs{
 	public static void main(String[] args){
 	System.out.println("x		y    ");
 	System.out.println("-------------------------");
-	for(double i=-10.0; i<=10.0; i++){
+	for(double i=-10.0; i<=10.0; i+=0.5){
 		System.out.println(i+"		"+(i*i));
 	}
 	}
