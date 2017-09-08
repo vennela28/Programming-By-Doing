f3f01f175d66f95e644eba33a80af9ec504875fb
vennela28 <vennela.jobs@gmail.com>
2017-09-08 15:28:30 -0700

    Safe Square Root

--- SafeSquareRoot/SafeSquareRoot.java
+++ SafeSquareRoot/SafeSquareRoot.java
@@ -7,12 +7,12 @@ public class SafeSquareRoot{
 	System.out.print("Enter a number: ");
 	int num=keyboard.nextInt();
 	
-	while(num<0){
+	do{
 		System.out.println("You can\'t take the square root of a negative number, silly.");
 		System.out.print("Try again: ");
 		num = keyboard.nextInt();
-
-	}
+	}while(num<0);
+	
 	double root=Math.sqrt(num);
 	System.out.println("The square root of "+num+" is "+root);
 	}
