e517f1ddd04d2838206810ed1c28bc186d47ca2e
vennela28 <vennela.jobs@gmail.com>
2017-09-13 22:32:35 -0700

    Random Numbers in an array

--- BasicArrays2/BasicArrays2.java
+++ BasicArrays2/BasicArrays2.java
@@ -3,9 +3,11 @@ public class BasicArrays2{
 	public static void main(String[] args){
 		Random r = new Random();
 		
+		//Declare an array of 10 integers
 		int a[]=new int[10];
+		
 		for(int i=0; i<a.length; i++){
-			a[i]=1 + (int)(Math.random()*100);
+			a[i]=1 + r.nextInt(100);
 			System.out.println("Slot "+i+" contains a "+a[i]);
 		}
 	}
