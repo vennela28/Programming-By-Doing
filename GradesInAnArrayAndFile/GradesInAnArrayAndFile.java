9a8de99aecd90c7274879db3ff265161bbc01d66
vennela28 <vennela.jobs@gmail.com>
2017-09-14 11:19:24 -0700

    Grades display in an Array and a File

---
+++ GradesInAnArrayAndFile/GradesInAnArrayAndFile.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+public class GradesInAnArrayAndFile{
+	public static void main(String[] args) throws Exception{
+		Scanner keyboard = new Scanner(System.in);
+		Random r = new Random();
+		int a[] = new int[5]; 
+		
+		System.out.print("Name (first last): ");
+		String fname = keyboard.next();
+		String lname = keyboard.next();
+		
+		System.out.print("Filename: ");
+		String file_name = keyboard.next();
+		
+		FileWriter fw = new FileWriter(file_name);
+		fw.write(fname+" "+lname);
+		fw.write("\n");
+		System.out.println();
+		System.out.println("Here are your randomly-selected grades (hope you pass):");
+		
+		for(int i=0; i<a.length; i++){
+			a[i]=1+r.nextInt(100);
+			fw.write(a[i] +"  ");
+			System.out.println("Grade "+(i+1)+" : "+a[i]);
+		}
+		fw.close();
+	}
+}
