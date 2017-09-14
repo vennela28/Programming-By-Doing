5c1cd4868ed3c4496724c4a7c8919d9a221c6945
vennela28 <vennela.jobs@gmail.com>
2017-09-13 22:41:12 -0700

    Change in the for loop structure

--- BasicArrays3/BasicArrays3.java
+++ BasicArrays3/BasicArrays3.java
@@ -3,9 +3,10 @@ public class BasicArrays3{
 	public static void main(String[] args){
 	int a[]=new int[1000];
 	Random r = new Random();
-	int x = 10 + r.nextInt(99);
+	
 	for(int i=0; i<a.length; i++){
-		System.out.print(10+r.nextInt(90)+"  ");
+		a[i] = 10 + r.nextInt(90);
+		System.out.print(a[i]+"  ");
 	}
 	}
 }
