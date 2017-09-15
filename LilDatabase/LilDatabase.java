2c7d243a507fb99ee8e67fc8b05a6e204369d979
vennela28 <vennela.jobs@gmail.com>
2017-09-15 10:06:36 -0700

    Records Student Database using arrays

--- LilDatabase/LilDatabase.java
+++ LilDatabase/LilDatabase.java
@@ -8,34 +8,38 @@ class Student
 public class LilDatabase{
 	public static void main(String[] args){
 		Scanner keyboard = new Scanner(System.in);
-		Student s_one = new Student();
-		Student s_two = new Student();
-		Student s_three = new Student();
+		Student s_db[] = new Student[3];
+		
+		s_db[0]=new Student();
 		System.out.print("Enter the first student's name: ");
-		 s_one.name = keyboard.next();
+		 s_db[0].name = keyboard.next();
 		System.out.print("Enter the first student's grade: ");
-		 s_one.grade = keyboard.nextInt();
+		 s_db[0].grade = keyboard.nextInt();
 		System.out.print("Enter the first student's average: ");
-		 s_one.avg = keyboard.nextDouble();
+		 s_db[0].avg = keyboard.nextDouble();
 		System.out.println();
+		
+		s_db[1]=new Student();
 		System.out.print("Enter the second student's name: ");
-		 s_two.name = keyboard.next();
+		 s_db[1].name = keyboard.next();
 		System.out.print("Enter the second student's grade: ");
-		 s_two.grade = keyboard.nextInt();
+		 s_db[1].grade = keyboard.nextInt();
 		System.out.print("Enter the second student's average: ");
-		 s_two.avg = keyboard.nextDouble();
+		 s_db[1].avg = keyboard.nextDouble();
 		System.out.println();
+		
+		s_db[2]=new Student();
 		System.out.print("Enter the third student's name: ");
-		s_three.name = keyboard.next();
+		s_db[2].name = keyboard.next();
 		System.out.print("Enter the third student's grade: ");
-		s_three.grade = keyboard.nextInt();
+		s_db[2].grade = keyboard.nextInt();
 		System.out.print("Enter the third student's average: ");
-		s_three.avg = keyboard.nextDouble();
+		s_db[2].avg = keyboard.nextDouble();
 		System.out.println();
 	
-		System.out.println("The names are: "+s_one.name+" "+s_two.name+" "+s_three.name);
-		System.out.println("The grades are: "+s_one.grade+" "+s_two.grade+" "+s_three.grade);
-		System.out.println("The averages are: "+s_one.avg+" "+s_two.avg+" "+s_three.avg);
+		System.out.println("The names are: "+s_db[0].name+" "+s_db[1].name+" "+s_db[2].name);
+		System.out.println("The grades are: "+s_db[0].grade+" "+s_db[1].grade+" "+s_db[2].grade);
+		System.out.println("The averages are: "+s_db[0].avg+" "+s_db[1].avg+" "+s_db[2].avg);
 		System.out.println();
 		
 	}
