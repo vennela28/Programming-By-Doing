7ea94984e4135527b78ab879e2cbbcadfa5c49c2
vennela28 <vennela.jobs@gmail.com>
2017-09-19 15:43:52 -0700

    Counting the prime numbers too.

--- FindingPrimeNumbers/FindingPrimeNumbers.java
+++ FindingPrimeNumbers/FindingPrimeNumbers.java
@@ -1,10 +1,16 @@
 public class FindingPrimeNumbers{
 	public static void main(String[] args){
+		int tries=0;
 		for(int i=2; i<=99; i++){
 			if(isPrime(i)){
+				System.out.println(i+ " <=");
+				tries++;
+			}else{
 				System.out.println(i);
 			}
 		}
+		System.out.println("\nThere are " +tries+ " prime numbers between 1 to 100.");
+			
 	}public static boolean isPrime(int n){
 		boolean prime=false;
 		for(int i=2; i<=n/2; i++){
