f4760139ff0cb933c0ae3d16bba5c050f655df10
vennela28 <vennela.jobs@gmail.com>
2017-09-27 11:56:17 -0700

    One shot high low

--- OneShotHiLo/OneShotHiLo.java
+++ OneShotHiLo/OneShotHiLo.java
@@ -5,8 +5,10 @@ public class OneShotHiLo{
 		int x = 1+r.nextInt(100);
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println("I\'m thinking of a number between 1-100.  Try to guess it.");
+		System.out.print("> ");
 		int guess = keyboard.nextInt();
 		System.out.println();
+		
 		if(x==guess){
 			System.out.println("You guessed it!  What are the odds?!?");
 		}else if(x>guess){
