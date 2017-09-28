fea8cacdc1840b07ad06cc9de5bfcc98ba0f24a9
vennela28 <vennela.jobs@gmail.com>
2017-09-28 14:47:01 -0700

    Calculate Area of the geometric shape desired

--- AreaCalculator/AreaCalculator.java
+++ AreaCalculator/AreaCalculator.java
@@ -3,8 +3,9 @@ public class AreaCalculator{
 	public static void main(String[] args){
 	Scanner keyboard = new Scanner(System.in);
 	System.out.println("Shape Area Calculator version 0.1 (c) 2005 Mitchell Sample Output, Inc.");
-	System.out.println();
+	
 	while(true){
+	System.out.println();
 	System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
 	System.out.println();
 	System.out.println("1) Triangle");
