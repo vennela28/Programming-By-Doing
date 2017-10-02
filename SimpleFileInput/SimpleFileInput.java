f4ea5435dde2526e7dcb7aec80ea0794c23228c0
vennela28 <vennela.jobs@gmail.com>
2017-10-02 13:31:08 -0700

    Simple File input - read the name from file read from the program.

--- SimpleFileInput/SimpleFileInput.java
+++ SimpleFileInput/SimpleFileInput.java
@@ -7,8 +7,9 @@ public class SimpleFileInput
 	{
 		
 		Scanner fileIn = new Scanner(new File("name.txt"));
-		String l_name =fileIn.next();
 		String f_name =fileIn.next();
+		String l_name =fileIn.next();
+		
 		
 		System.out.println( "Using my psychic powers aided by reading data from the file, I have" );
 		System.out.println( "determined that your name is " +f_name+" "+l_name+"." );
