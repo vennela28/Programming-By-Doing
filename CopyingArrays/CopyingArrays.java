ec514c8f80b414e16c183f1974a3cb27d24431f6
vennela28 <vennela.jobs@gmail.com>
2017-10-02 15:40:07 -0700

    Copying Arrays

--- CopyingArrays/CopyingArrays.java
+++ CopyingArrays/CopyingArrays.java
@@ -5,24 +5,18 @@ public class CopyingArrays{
 		int x = 1+r.nextInt(100);
 		int arr[] = new int[10];
 		int arr1[] = new int[10];
-		
+		System.out.print("Array 1: ");
 		for(int a=0; a<arr.length; a++){
-			int num = 1+r.nextInt(100);
-			arr[a]=num;
-	}		arr1 = arr;
-	
-	System.out.print("Array 1: ");
-	for(int a=0; a<arr.length; a++){
-		System.out.print(arr[a]+"  ");
-	}	arr[arr.length - 1] = -7;
+			arr[a] = 1+r.nextInt(100);
+			System.out.print(arr[a]+"  ");
+		}
+		arr1 = arr;
+		arr[arr.length - 1] = -7;
 		System.out.println();
-	System.out.print("Array 2: ");
-	for(int b=0; b<arr1.length; b++){
-		System.out.print(arr1[b]+"  ");
 		
-	}	
-		
-		
-		
+		System.out.print("Array 2: ");
+		for(int b=0; b<arr1.length; b++){
+			System.out.print(arr1[b]+"  ");
+		}	
 	}
 }
