65132b4d0b52a272f6a02f627825d66bb0f470dd
vennela28 <vennela.jobs@gmail.com>
2017-10-02 14:08:03 -0700

    Sum Several numbers from the file specified.

--- SummingSeveralNumbersFromAnyFile/SummingSeveralNumbersFromAnyFile.java
+++ SummingSeveralNumbersFromAnyFile/SummingSeveralNumbersFromAnyFile.java
@@ -1,21 +1,29 @@
-import java.util.*;
-import java.io.*;
-public class SummingSeveralNumbersFromAnyFile{
-	public static void main(String args[]) throws Exception{
-		Scanner keyboard = new Scanner(System.in);
-	    String usrFile;
-		System.out.print( "Which file would you like to read numbers from: " );
-		usrFile = keyboard.next();
-		File in = new File(usrFile);
-		Scanner input = new Scanner(in);
-		System.out.println( "Reading numbers from file \"" + usrFile + "\"\n" );
-		int total = 0;
-		while ( input.hasNext() )
-		{
-		    int num = input.nextInt();
-		    System.out.print( num + " " );
-		    total += num;		
-		}
-		System.out.println( "\nTotal is " + total);
-	}
-}
+  import java.io.File;
+    import java.util.Scanner;
+    
+    public class SummingSeveralNumbersFromAnyFile {
+    
+        public static void main(String[] args) throws Exception {
+        
+            String file;
+            int total = 0;
+            
+            Scanner k = new Scanner(System.in);
+            
+            System.out.print("\nWhich file would you like to read numbers from: ");
+            file = k.next();
+            System.out.println("Reading numbers from \"" + file + "\"");
+            System.out.println();
+            
+            Scanner fileReader = new Scanner(new File(file));
+            
+            while(fileReader.hasNext()) {
+                int number = fileReader.nextInt();
+                System.out.print(number + " ");
+                total = number + total;
+            }
+            
+            System.out.println("\nTotal is " + total + "\n");
+            fileReader.close();
+        }
+    }
