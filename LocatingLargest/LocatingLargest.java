7b4e357b02386182ef4ef6d1d9ef17fa43625947
vennela28 <vennela.jobs@gmail.com>
2017-10-02 16:17:19 -0700

    Find out the largest number in the array and display its slot number.

---
+++ LocatingLargest/LocatingLargest.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+public class LocatingLargest{
+	public static void main(String[] args){
+		Scanner keyboard = new Scanner(System.in);
+		Random r = new Random();
+		int a[] = new int[10];
+		int largest=0;
+		int slot=0;
+		System.out.print("Array: ");
+		for(int i=0; i<a.length; i++){
+			a[i]= 1+r.nextInt(50);
+			System.out.print(a[i] +"  ");
+		}
+		for(int i=0; i<a.length-1; i++){
+			if(largest<a[i]){
+				largest=a[i];
+				slot=i;
+			}
+		}
+		System.out.println();
+		System.out.println("\nThe largest value is "+largest+".");
+		System.out.println("It is found in slot "+slot+".");
+	}
+}
