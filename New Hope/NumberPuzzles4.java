4631053ebfd97520c6d2f8dc7043b8a3e48621ae
vennela28 <vennela.jobs@gmail.com>
2017-10-02 13:18:26 -0700

    Number Puzzle whose sum is 45.

--- New Hope/NumberPuzzles4.java
+++ New Hope/NumberPuzzles4.java
@@ -5,7 +5,7 @@ public class NumberPuzzles4{
 			for(int c=1; c<45; c++){
 				for(int d=1; d<45; d++){
 					if ( (a + b + c + d) == 45 && (a + 2) == (b - 2) && (b - 2) == (c * 2) && (c *2) == (d / 2) )
-						System.out.println( a + " + " + b + " + " + c + " + " + d );
+						System.out.println( a + " + " + b + " + " + c + " + " + d +" = 45");
 				}
 			}
 		}
