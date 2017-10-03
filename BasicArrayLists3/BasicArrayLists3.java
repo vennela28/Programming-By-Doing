f87b6955193e1cc18cb4978f80c05ac4f84b8005
vennela28 <vennela.jobs@gmail.com>
2017-10-03 13:39:51 -0700

    Add 1000 random numbers in an array list and display them.

--- BasicArrayLists3/BasicArrayLists3.java
+++ BasicArrayLists3/BasicArrayLists3.java
@@ -1,16 +1,23 @@
 import java.util.*;
 
 public class BasicArrayLists3{
-	public static void main(String[] args){
+	public static void main(String[] args)
+	{
+		printArray(1000);
+	}
+	
+	static void printArray(int size){
 	ArrayList<Integer> al = new ArrayList<Integer>();
-	for(int i=1; i<=1000; i++){
+	for(int i=0; i<size; i++){
 		al.add(getNumbers());
 		
-	}System.out.println("ArrayList: "+al);
+		}System.out.print("Array List: "+al+"\t");
 	}
-	public static int getNumbers(){
+	
+	static int getNumbers(){
 		Random r = new Random();
 		int x = r.nextInt(90) + 10;
 		return x;
 	}
+	
 }
